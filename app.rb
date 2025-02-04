fbe42128e0457c2889784f532f0fcbd4a7ba8089
AndreasSameus <andreas.sameus@elev.ga.ntig.se>
2025-02-04 10:13:31 +0100

    v1.1
    entries

--- app.rb
+++ app.rb
@@ -7,3 +7,40 @@ require 'bcrypt'
 get('/')  do
     slim(:home)
 end 
+
+get("/entries") do
+    title = params[:title]
+    genre = params[:genre]
+    type = params[:type]
+    creator = params[:creator]
+    date = params[:date]
+    ASC = params[:ASC]
+    DESC = params[:DESC]
+
+    if genre
+        main_order = genre
+    elsif type
+        main_order = type
+    elsif creator
+        main_order = creator
+    elsif date
+        main_order = date
+    else
+        main_order = "title"
+    end
+
+    if DESC
+        hej = DESC
+    else
+        hej = "ASC"
+    end
+    
+    p main_order
+    p hej
+
+    db = SQLite3::Database.new("db/Sameusboxd.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM entries ORDER BY ? ?",[main_order,hej])
+    slim(:"entries",locals:{entries:result})
+  
+end
