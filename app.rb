0e9468af0be7b716ceb5042f3b52a6a53762e229
AndreasSameus <andreas.sameus@elev.ga.ntig.se>
2025-03-18 10:07:24 +0100

    v1.5
    added a profile and rating system both need to be refined

--- app.rb
+++ app.rb
@@ -131,3 +131,24 @@ post("/create") do
     result = db.execute("INSERT INTO entries (title,creator,type,genre,date,img) VALUES(?,?,?,?,?,?)",[entryname,creator,type,genre,date,filename])
     redirect("/create")
 end
+
+get("/profile/:username") do
+    db = SQLite3::Database.new("db/Sameusboxd.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT id FROM users WHERE username = ?",params[:username])
+    p result
+    slim(:"profile",locals:{profile:result})
+end
+
+post("/entries/rate") do
+    db = SQLite3::Database.new("db/Sameusboxd.db")
+    db.results_as_hash = true
+    user_id = db.execute("SELECT id FROM users WHERE username = ?",session[:user]).first["id"]
+    entry_id = params[:entry_id].to_i
+    rating = params[:rating].to_i
+    p rating
+    p user_id
+    p entry_id
+    db.execute("INSERT INTO user_entry_rel (user_id,entry_id,rating) VALUES (?,?,?)",[user_id,entry_id,rating])
+    redirect("/entries/#{params[:entry_id]}")
+end
